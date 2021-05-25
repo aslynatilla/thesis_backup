f674cd3ecaad6cf27e4685977faa7756540fa4df
Antonio Natilla <antonio.natilla@studenti.unimi.it>
2021-05-25 12:10:40 +0200

    Implementation for interpolation
    TODO: extract the lambda in a specific struct - maybe Interpolator is a
    good name? maybe just move the logic in BlockInfo? - so that the code is
    cleaner.
    
    TODO: move computing points after interpolation in another function,
    giving it the number of points on the edges, the interpolator structure
    and a reference to the block we're filling.

--- src/ies/adapter/mesh_interpolation.cpp
+++ src/ies/adapter/mesh_interpolation.cpp
@@ -13,7 +13,7 @@ namespace ies::adapter{
                 const bool is_last_row = (row == dimensions.height - 2);
                 const bool is_last_column = (col == dimensions.width - 2);
                 auto block = reserve_block_space(is_last_row, is_last_column, new_points_per_edge);
-                auto block_inserter = std::back_inserter(block);
+
                 BlockDescriptor block_description{
                     .block_corners = {.p00 = angles[row][col],
                                       .p01 = angles[row][col+1],
@@ -25,14 +25,15 @@ namespace ies::adapter{
                                       .p11 = points[row+1][col+1]},
 
                 };
+
                 if(is_last_column && is_last_row){
-                    last_interpolated_block(block_description, new_points_per_edge, block_inserter);
+                    last_interpolated_block(block_description, new_points_per_edge, block);
                 } else if(is_last_column){
-                    interpolated_block_on_last_col(block_description, new_points_per_edge, block_inserter);
+                    interpolated_block_on_last_col(block_description, new_points_per_edge, block);
                 } else if( is_last_row){
-                    interpolated_block_on_last_row(block_description, new_points_per_edge, block_inserter);
+                    interpolated_block_on_last_row(block_description, new_points_per_edge, block);
                 } else {
-                    interpolated_block(block_description, new_points_per_edge, block_inserter);
+                    interpolated_block(block_description, new_points_per_edge, block);
                 }
                 grid_of_blocks.push_back(block);
             }
@@ -46,6 +47,14 @@ namespace ies::adapter::impl_details{
     GridDimension GridDimension::from(const vec3_grid& target_grid) {
         return GridDimension{.width = target_grid[0].size(),
                              .height = target_grid.size()};
+    }
+
+    glm::vec2
+    BlockDescriptor::compute_interpolation_step(unsigned short points_per_x_edge, unsigned short points_per_y_edge) const {
+        const auto x_edge_points = static_cast<float>(points_per_x_edge);
+        const auto y_edge_points = static_cast<float>(points_per_y_edge);
+        return glm::vec2((block_corners.p10.x - block_corners.p00.x)/x_edge_points,
+                        (block_corners.p01.y - block_corners.p00.y)/y_edge_points);
     }
 
     std::vector<glm::vec3>
@@ -62,38 +71,105 @@ namespace ies::adapter::impl_details{
     }
 
     void last_interpolated_block(BlockDescriptor block_info, unsigned short new_points,
-                                 std::back_insert_iterator<std::vector<glm::vec3>> block_inserter) {
-        //  Dummy implementation
-        const int block_dim = (new_points + 2) * (new_points + 2);
-        for(int i = 0; i < block_dim; ++i){
-            block_inserter = block_info.f_in_corners.p00;
+                                 std::vector<glm::vec3>& block) {
+        const int x_edge_points = new_points + 2;
+        const int y_edge_points = new_points + 2;
+
+        auto interpolator = [&block_info](const glm::vec2 interpolation_point) -> glm::vec3 {
+            const auto& angles = block_info.block_corners;
+            const auto& f = block_info.f_in_corners;
+            const float denominator = (angles.p11.x - angles.p00.x) * (angles.p11.y - angles.p00.y);
+            const auto term00 = f.p00 * (angles.p11.x - interpolation_point.x) * (angles.p11.y - interpolation_point.y);
+            const auto term10 = f.p10 * (interpolation_point.x - angles.p00.x) * (angles.p11.y - interpolation_point.y);
+            const auto term01 = f.p01 * (angles.p11.x - interpolation_point.x) * (interpolation_point.y - angles.p00.y);
+            const auto term11 = f.p11 * (interpolation_point.x - angles.p00.x) * (interpolation_point.y - angles.p00.y);
+            return (term00 + term01 + term10 + term11)/denominator;
+        };
+
+        const auto step = block_info.compute_interpolation_step(x_edge_points, y_edge_points);
+        for(auto i = 0; i < y_edge_points; ++i){
+            auto y = block_info.block_corners.p00.y + step.y * i;
+            for(auto j = 0; j < x_edge_points; ++j){
+                auto x = block_info.block_corners.p00.x + step.x * j;
+                block.push_back(interpolator(glm::vec2(x, y)));
+            }
         }
     }
 
     void interpolated_block_on_last_col(BlockDescriptor block_info, unsigned short new_points,
-                                        std::back_insert_iterator<std::vector<glm::vec3>> block_inserter) {
-        //  Dummy implementation
-        const int block_dim = (new_points + 2) * (new_points + 1);
-        for(int i = 0; i < block_dim; ++i){
-            block_inserter = block_info.f_in_corners.p00;
+                                        std::vector<glm::vec3>& block) {
+        const int x_edge_points = new_points + 2;
+        const int y_edge_points = new_points + 1;
+
+        auto interpolator = [&block_info](const glm::vec2 interpolation_point) -> glm::vec3 {
+            const auto& angles = block_info.block_corners;
+            const auto& f = block_info.f_in_corners;
+            const float denominator = (angles.p11.x - angles.p00.x) * (angles.p11.y - angles.p00.y);
+            const auto term00 = f.p00 * (angles.p11.x - interpolation_point.x) * (angles.p11.y - interpolation_point.y);
+            const auto term10 = f.p10 * (interpolation_point.x - angles.p00.x) * (angles.p11.y - interpolation_point.y);
+            const auto term01 = f.p01 * (angles.p11.x - interpolation_point.x) * (interpolation_point.y - angles.p00.y);
+            const auto term11 = f.p11 * (interpolation_point.x - angles.p00.x) * (interpolation_point.y - angles.p00.y);
+            return (term00 + term01 + term10 + term11)/denominator;
+        };
+
+        const auto step = block_info.compute_interpolation_step(x_edge_points, y_edge_points);
+        for(auto i = 0; i < y_edge_points; ++i){
+            auto y = block_info.block_corners.p00.y + step.y * i;
+            for(auto j = 0; j < x_edge_points; ++j){
+                auto x = block_info.block_corners.p00.x + step.x * j;
+                block.push_back(interpolator(glm::vec2(x, y)));
+            }
         }
     }
 
     void interpolated_block_on_last_row(BlockDescriptor block_info, unsigned short new_points,
-                                        std::back_insert_iterator<std::vector<glm::vec3>> block_inserter) {
-        //  Dummy implementation
-        const int block_dim = (new_points + 2) * (new_points + 1);
-        for(int i = 0; i < block_dim; ++i){
-            block_inserter = block_info.f_in_corners.p00;
+                                        std::vector<glm::vec3>& block) {
+        const int x_edge_points = new_points + 1;
+        const int y_edge_points = new_points + 2;
+
+        auto interpolator = [&block_info](const glm::vec2 interpolation_point) -> glm::vec3 {
+            const auto& angles = block_info.block_corners;
+            const auto& f = block_info.f_in_corners;
+            const float denominator = (angles.p11.x - angles.p00.x) * (angles.p11.y - angles.p00.y);
+            const auto term00 = f.p00 * (angles.p11.x - interpolation_point.x) * (angles.p11.y - interpolation_point.y);
+            const auto term10 = f.p10 * (interpolation_point.x - angles.p00.x) * (angles.p11.y - interpolation_point.y);
+            const auto term01 = f.p01 * (angles.p11.x - interpolation_point.x) * (interpolation_point.y - angles.p00.y);
+            const auto term11 = f.p11 * (interpolation_point.x - angles.p00.x) * (interpolation_point.y - angles.p00.y);
+            return (term00 + term01 + term10 + term11)/denominator;
+        };
+
+        const auto step = block_info.compute_interpolation_step(x_edge_points, y_edge_points);
+        for(auto i = 0; i < y_edge_points; ++i){
+            auto y = block_info.block_corners.p00.y + step.y * i;
+            for(auto j = 0; j < x_edge_points; ++j){
+                auto x = block_info.block_corners.p00.x + step.x * j;
+                block.push_back(interpolator(glm::vec2(x, y)));
+            }
         }
     }
 
     void interpolated_block(BlockDescriptor block_info, unsigned short new_points,
-                            std::back_insert_iterator<std::vector<glm::vec3>> block_inserter) {
-        //  Dummy implementation
-        const int block_dim = (new_points + 1) * (new_points + 1);
-        for(int i = 0; i < block_dim; ++i){
-            block_inserter = block_info.f_in_corners.p00;
+                            std::vector<glm::vec3>& block) {
+        const int edge_points = new_points + 1;
+
+        auto interpolator = [&block_info](const glm::vec2 interpolation_point) -> glm::vec3 {
+            const auto& angles = block_info.block_corners;
+            const auto& f = block_info.f_in_corners;
+            const float denominator = (angles.p11.x - angles.p00.x) * (angles.p11.y - angles.p00.y);
+            const auto term00 = f.p00 * (angles.p11.x - interpolation_point.x) * (angles.p11.y - interpolation_point.y);
+            const auto term10 = f.p10 * (interpolation_point.x - angles.p00.x) * (angles.p11.y - interpolation_point.y);
+            const auto term01 = f.p01 * (angles.p11.x - interpolation_point.x) * (interpolation_point.y - angles.p00.y);
+            const auto term11 = f.p11 * (interpolation_point.x - angles.p00.x) * (interpolation_point.y - angles.p00.y);
+            return (term00 + term01 + term10 + term11)/denominator;
+        };
+
+        const auto step = block_info.compute_interpolation_step(edge_points, edge_points);
+        for(auto i = 0; i < edge_points; ++i){
+            auto y = block_info.block_corners.p00.y + step.y * i;
+            for(auto j = 0; j < edge_points; ++j){
+                auto x = block_info.block_corners.p00.x + step.x * j;
+                block.push_back(interpolator(glm::vec2(x, y)));
+            }
         }
     }
 
--- src/ies/adapter/mesh_interpolation.h
+++ src/ies/adapter/mesh_interpolation.h
@@ -33,22 +33,25 @@ namespace ies::adapter {
         struct BlockDescriptor {
             Rect<glm::vec2> block_corners;
             Rect<glm::vec3> f_in_corners;
+
+            [[nodiscard]] glm::vec2 compute_interpolation_step(unsigned short points_per_x_edge,
+                                                 unsigned short points_per_y_edge) const;
         };
 
         std::vector<glm::vec3>
         reserve_block_space(bool is_last_row_block, bool is_last_column_block, uint16_t new_points_per_edge);
 
         void last_interpolated_block(BlockDescriptor block_info, unsigned short new_points,
-                                     std::back_insert_iterator<std::vector<glm::vec3>> block_inserter);
+                                     std::vector<glm::vec3>& block);
 
         void interpolated_block_on_last_col(BlockDescriptor block_info, unsigned short new_points,
-                                            std::back_insert_iterator<std::vector<glm::vec3>> block_inserter);
+                                            std::vector<glm::vec3>& block);
 
         void interpolated_block_on_last_row(BlockDescriptor block_info, unsigned short new_points,
-                                            std::back_insert_iterator<std::vector<glm::vec3>> block_inserter);
+                                            std::vector<glm::vec3>& block);
 
         void interpolated_block(BlockDescriptor block_info, unsigned short new_points,
-                                std::back_insert_iterator<std::vector<glm::vec3>> block_inserter);
+                                std::vector<glm::vec3>& block);
 
         vec3_grid compose_interpolated_grid(GridDimension dimension, uint16_t new_points_per_edge, vec3_grid&& blocks);
     }
